91b0b9a56f6fafeadc923647e12f68dd85823891
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-10-02 21:13:35 +0300

    marks

---
+++ main.py
@@ -0,0 +1,15 @@
+student_name = "Alizecoolpro"
+marks = {
+    "Boodyzecoolpro": 0,
+    "Mezecoolpro": 1,
+    "omarzecoolpro":0,
+    "larazecoolpro":0,
+    "adamzecoolpro":0,
+    "Alizecoolpro":0,
+    "mrmusabkhanzecoollpro":100,
+    "mrmoatazzecoolpro":0
+}
+
+for student in marks:
+    if student== student_name:
+        print(marks[student])
