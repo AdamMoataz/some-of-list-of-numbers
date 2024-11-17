877e167dc61f85f00f1eeb48a1a287ee5e67d228
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-11-17 20:13:32 +0200

    i learnt how to use for loop and range

---
+++ practice.py
@@ -0,0 +1,33 @@
+"""
+print the number from 1 too 10 using the for loop
+"""
+
+for i in range(1,11):
+    print(i)
+"""
+print the number from 1 to 10 in the form of list
+"""
+print(list(range(1,11)))   
+"""
+print the number from 3 to 15
+"""
+print(list(range(3,14)))
+
+for i in range(3,16):
+    print(i)
+
+"""
+print the number from 2 to 10 by skipping 1 number inbetween
+"""
+print(list(range(2,11,2)))
+
+for i in range(2,11,2):
+    print(i)
+
+
+"""
+print the number from 2 to 20 by skipping three numbers
+"""
+print(list(range(2,21,4)))
+for i in range(2,21,4):
+    print(i)
